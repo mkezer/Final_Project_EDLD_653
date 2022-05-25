82afc44bd3e8f80e73b62213b89945ee81ec9f0f
Murat <kezermurat@gmail.com>
2022-05-24 20:34:18 -0700

    items and vars for reliability and descriptives

---
+++ items_reliability.R
@@ -0,0 +1,68 @@
+# items
+insecure_att <- c("as1rc", "as2", "as3rc", "as4rc", "as5")
+avoid_att <- c("as6", "as7", "as8", "as9")
+anx_att <- c("as10", "as11", "as12rc", "as13")
+
+authenticity <- c("auth1", "auth2rc", "auth3rc", "auth4rc", 
+                  "auth5rc", "auth6rc", "auth7rc", "auth8", 
+                  "auth9", "auth10rc", "auth11", "auth12rc")
+
+masking <- c("mask1", "mask2", "mask3", "mask4", "mask5", 
+             "mask6", "mask7", "mask8", "mask9", "mask10rc", 
+             "mask11", "mask12", "mask13")
+
+ntb <- c("ntb1rc", "ntb2", "ntb3rc", "ntb4", "ntb5", 
+         "ntb6", "ntb7rc", "ntb8", "ntb9", "ntb10")
+
+loneliness <- c("ucla1rc", "ucla2", "ucla3", "ucla4", "ucla5rc", 
+                "ucla6rc", "ucla7", "ucla8", "ucla9rc", "ucla10rc", 
+                "ucla11", "ucla12", "ucla13", "ucla14", "ucla15rc", 
+                "ucla16rc", "ucla17", "ucla18", "ucla19rc", "ucla20rc")
+
+fne <- paste0("fne", 1:12)
+
+pos_express <- c('beq1', 'beq4', 'beq6', 'beq10')
+neg_express <- c('beq3rc', 'beq5', 'beq8rc', 'beq9rc', 'beq13', 'beq16')
+impulse_str <- c('beq2', 'beq7', 'beq11', 'beq12', 'beq14', 'beq15')
+
+suppression <- c('erq2', 'erq4', 'erq6', 'erq9')
+reappraisal <- c('erq1', 'erq3', 'erq5', 'erq7', 'erq8', 'erq10')
+
+s_esteem <- c('rse1', 'rse2', 'rse3rc', 'rse4', 'rse5rc', 
+              'rse6', 'rse7', 'rse8rc', 'rse9rc', 'rse10rc')
+
+swb <- paste0('swb', 1:5)
+
+sense_of_power <- c('pow1', 'pow2rc', 'pow3', 'pow4rc', 
+                    'pow5', 'pow6rc', 'pow7rc', 'pow8')
+
+si_anx <- paste0('sias', 1:6)
+s_phobia <- paste0('sias', 7:12)
+
+## lists
+item_list <- list(insecure_att, avoid_att, anx_att, authenticity,
+                  masking, ntb, loneliness, fne, pos_express,
+                  neg_express, impulse_str, suppression, reappraisal,
+                  s_esteem, swb, sense_of_power, si_anx, s_phobia)
+
+variable_names <- list("Insecure Attachment", 
+                       "Avoidant Attachment",
+                       "Anxious Attachment",
+                       "Authenticity",
+                       "Masking",
+                       "Need to Belong",
+                       "Loneliness",
+                       "Fear of Negative Evaluation",
+                       'Positive Expressivity',
+                       'Negative Expressivity',
+                       'Impulse Strength',
+                       'Supression',
+                       'Reappraisal',
+                       'Self-Esteem',
+                       'Satisfaction with Life',
+                       'Sense of Power',
+                       'Social Interaction Anxiety',
+                       'Social Phobia')
+
+
+
